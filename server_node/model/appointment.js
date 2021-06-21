02ff7d43a17ef4e1969ff4ee9bc2edbddbe4c733
muskansingh11 <musksingh111099@gmail.com>
2021-06-21 10:42:20 +0530

    appointment model created

---
+++ server_node/model/appointment.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const {Slots,valid} = require('./slots');
+const {User, validate} = require('./user');
+const {Doctor,validate} = require('./doctor');
+const appointmentSchema =  new mongoose.Schema({
+
+    userid:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    doctorid:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Doctor',
+    },
+    slotid:{
+        type:String
+    },
+    isConfirmedByDoctor:
+    {
+        type:Boolean,
+        default:false
+    }
+})
+
+
+const appointment = mongoose.model('appointment', appointmentSchema);
+exports.appointment = appointment;
